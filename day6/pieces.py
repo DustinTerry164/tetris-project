7f79935287f217dc6ae8de8520525c747d657a51
DustinTerry164 <75955702+DustinTerry164@users.noreply.github.com>
2023-05-08 19:15:55 -0700

    Add files via upload

--- day6/pieces.py
+++ day6/pieces.py
@@ -71,8 +71,6 @@ can_move_right = False
 can_move_left = False
 target_piece = "Long_piece"
 next_piece = "Cube_piece"
-piece_list_names = ["Long_piece", "T_piece", "Cube_piece", "L_piece", "RL_piece", "S_piece", "Z_piece"]
-piece_list_object = [Long_piece, T_piece, Cube_piece, L_piece, RL_piece, S_piece, Z_piece]
 piece_rotation = 0  # determines rotation of piece
 max_rotation = 1  # determines how many times a piece can rotate
 current_piece = Long_piece[0]
@@ -80,10 +78,11 @@ current_piece = Long_piece[0]
 
 def randomized_piece():
     global next_piece
+    names = ["Long_piece", "T_piece", "Cube_piece", "L_piece", "RL_piece", "S_piece", "Z_piece"]
     random_value = random.randrange(0, 7, 1)
-    next_piece = piece_list_names[random_value]
+    next_piece = names[random_value]
     if (random_value == 6 or random_value == 5):
-        if (random.randrange(0, 2, 1) == 0): next_piece = piece_list_names[random.randrange(0, 7, 1)]
+        if (random.randrange(0, 2, 1) == 0): next_piece = names[random.randrange(0, 7, 1)]
 
 
 def rotate(direction):
@@ -107,31 +106,32 @@ def set_piece_target(wanted_piece):
 
 def update_piece():
     global current_piece, max_rotation
+    objects = [Long_piece, T_piece, Cube_piece, L_piece, RL_piece, S_piece, Z_piece]
     match target_piece:
 
         case "Long_piece":
-            max_rotation = len(piece_list_object[0]) - 1
-            current_piece = piece_list_object[0][piece_rotation]
+            max_rotation = len(objects[0]) - 1
+            current_piece = objects[0][piece_rotation]
 
         case "T_piece":
-            max_rotation = len(piece_list_object[1]) - 1
-            current_piece = piece_list_object[1][piece_rotation]
+            max_rotation = len(objects[1]) - 1
+            current_piece = objects[1][piece_rotation]
 
         case "Cube_piece":
-            max_rotation = len(piece_list_object[2]) - 1
-            current_piece = piece_list_object[2][piece_rotation]
+            max_rotation = len(objects[2]) - 1
+            current_piece = objects[2][piece_rotation]
         case "L_piece":
-            max_rotation = len(piece_list_object[3]) - 1
-            current_piece = piece_list_object[3][piece_rotation]
+            max_rotation = len(objects[3]) - 1
+            current_piece = objects[3][piece_rotation]
         case "RL_piece":
-            max_rotation = len(piece_list_object[4]) - 1
-            current_piece = piece_list_object[4][piece_rotation]
+            max_rotation = len(objects[4]) - 1
+            current_piece = objects[4][piece_rotation]
         case "S_piece":
-            max_rotation = len(piece_list_object[5]) - 1
-            current_piece = piece_list_object[5][piece_rotation]
+            max_rotation = len(objects[5]) - 1
+            current_piece = objects[5][piece_rotation]
         case "Z_piece":
-                max_rotation = len(piece_list_object[6]) - 1
-                current_piece = piece_list_object[6][piece_rotation]
+                max_rotation = len(objects[6]) - 1
+                current_piece = objects[6][piece_rotation]
 
 
 
